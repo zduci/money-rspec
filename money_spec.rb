489ea95e07d4cf1d62cb899866aee6909c955e0e
Radu Busuioc <zduci@yahoo.com>
2012-02-16 20:34:17 +0000

    refactored

--- money_spec.rb
+++ money_spec.rb
@@ -3,15 +3,13 @@ require './dollar'
 describe Dollar do
   it 'correctly multiplies by anything' do
     dollar = Dollar.new 5
-    product = dollar.times 2
-    product.should == Dollar.new(10)
-    product = dollar.times 3
-    product.should == Dollar.new(15) 
+    dollar.times(2) == Dollar.new(10)
+    dollar.times(3) == Dollar.new(15) 
   end
   
   it 'does equality' do 
     dollar = Dollar.new 5
-    dollar.should == (Dollar.new 5)
+    dollar.should == Dollar.new(5)
     dollar.should_not == Dollar.new(6)
   end
 end
